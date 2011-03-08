f1aa2fe1686fe23aacbeb3a0f3a1a05888fbdad9
Dave Elkan <dave@edave.net>
2011-03-09 07:46:07 +1100

    Enabled MongoDB.  - Replaced in-memory database with MongoDB.  - Took a trip via Mongoose to find it didn't really fit with what I was trying to do (that is, corralling sources of error).

--- app.js
+++ app.js
@@ -5,6 +5,11 @@
 
 var express = require('express'),
     app = module.exports = express.createServer(),
+    Db = require('mongodb').Db,
+    ObjectId = require('mongodb').ObjectId,
+    Server = require('mongodb').Server,
+    GenericPool = require('generic-pool'),
+    
     notFoundHandler = require("./lib/middleware/NotFoundHandler"),
     BookService = require("./lib/service/BookService").BookService,
     BookController = require('./lib/controller/BookController').BookController,
@@ -12,6 +17,21 @@ var express = require('express'),
     AuthorController = require('./lib/controller/AuthorController').AuthorController,
     HomeService = require("./lib/service/HomeService").HomeService,
     HomeController = require('./lib/controller/HomeController').HomeController;
+    
+app.pool = GenericPool.Pool({
+    max: 10,
+    create: function(callback) {
+        var db = new Db('layerednode', new Server("127.0.0.1", 27017, {}));
+        db.open(function(err, p_client) {
+            callback(db);                
+        });
+    },
+    log: false,
+    idleTimeOutMillis: 500,
+    destroy: function(client) {
+        client.close();
+    }
+});
 
 // Configuration
 
@@ -44,10 +64,14 @@ app.bookService = new BookService(app);
 app.authorService = new AuthorService(app);
 app.homeService = new HomeService(app);
 
+// Bootstrap Content
+require("./lib/bootstrap").bootstrap(app);
+
 // Routes
 new BookController(app);
 new AuthorController(app);
 new HomeController(app);
+
 
 // Only listen on $ node app.js
 
