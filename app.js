8e87689f286c8fa3a0b83725666c3a8a475e4c0b
Dave Elkan <dave@edave.net>
2011-04-13 07:45:37 +1000

    Routes work
    - Route now abstracted from controller
    - Views abstracted from controller

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -30,22 +29,26 @@ app.pool = GenericPool.Pool({
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-  app.use(notFoundHandler);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+    app.use(notFoundHandler);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+app.configure('development', function() {
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
+app.configure('production', function() {
+    app.use(express.errorHandler()); 
 });
+
+function getInstanceName(className) {
+    return className[0].toLowerCase() + className.substr(1);
+}
 
 /**
  * Services and Controllers.
@@ -56,6 +59,48 @@ app.configure('production', function(){
  */
 loader(app, "services");
 loader(app, "controllers");
+
+var controllers = {},
+    routes = require('./lib/routes/routes')(app),
+    baseFielder = {
+        method: "get",
+        headers: {
+            "accept": "text/html"
+        }
+    };
+for (var path in routes) {
+    var fielders = routes[path];
+    fielders.forEach(setupRoute);
+}
+
+function setupRoute(fielder) {
+    fielder = merge(baseFielder, fielder);
+    app[fielder.method].call(app, path, [], function(req, res, next) {
+        if (req.accepts(fielder.headers.accept)) {
+            var args = Array().slice.apply(arguments);
+            args.push({
+                fielder: fielder,
+                path: path
+            });
+            fielder.action.apply(app, args);
+            return;
+        } else {
+            next();
+        }
+    });
+}
+
+function merge(base, inst) {
+    var merged = {};
+    for (var p in base) {
+        merged[p] = base[p];
+    }
+    for (var p in inst) {
+        merged[p] = inst[p];
+    }
+    
+    return merged;
+}
 
 // Bootstrap Content
 require("./lib/bootstrap")(app);
